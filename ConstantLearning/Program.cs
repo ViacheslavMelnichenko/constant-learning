71fd1cabf84ace5b3c191cc895e2780cce0c8f88
Viacheslav Melnichenko <melnichenkoviatcheslav@gmail.com>
2026-01-30 04:58:52 +0100

    rework options usage

--- ConstantLearning/Program.cs
+++ ConstantLearning/Program.cs
@@ -19,21 +19,26 @@ public static class Program
         builder.Services.Configure<TelegramOptions>(builder.Configuration.GetSection(TelegramOptions.SectionName));
         builder.Services.Configure<ScheduleOptions>(builder.Configuration.GetSection(ScheduleOptions.SectionName));
         builder.Services.Configure<LearningOptions>(builder.Configuration.GetSection(LearningOptions.SectionName));
-        builder.Services.Configure<WordsImportOptions>(builder.Configuration.GetSection(WordsImportOptions.SectionName));
+        builder.Services.Configure<WordsImportOptions>(
+            builder.Configuration.GetSection(WordsImportOptions.SectionName));
         builder.Services.Configure<LanguageOptions>(builder.Configuration.GetSection(LanguageOptions.SectionName));
 
         // Database
         var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
-            ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found");
+                               ?? throw new InvalidOperationException(
+                                   "Connection string 'DefaultConnection' not found");
 
         builder.Services.AddDbContext<AppDbContext>(options =>
             options.UseNpgsql(connectionString));
 
         // Telegram Bot
-        var botToken = builder.Configuration["Telegram:BotToken"]
-            ?? throw new InvalidOperationException("Telegram:BotToken is not configured");
+        var telegramOptions = builder
+                                  .Configuration
+                                  .GetSection(TelegramOptions.SectionName)
+                                  .Get<TelegramOptions>()
+                              ?? throw new InvalidOperationException("Telegram configuration not found");
 
-        builder.Services.AddSingleton<ITelegramBotClient>(new TelegramBotClient(botToken));
+        builder.Services.AddSingleton<ITelegramBotClient>(new TelegramBotClient(telegramOptions.BotToken));
         builder.Services.AddScoped<ITelegramBotService, TelegramBotService>();
 
         // Services
@@ -44,29 +49,32 @@ public static class Program
         builder.Services.AddScoped<IChatRegistrationService, ChatRegistrationService>();
 
         // Quartz Scheduler
+        var scheduleOptions = builder
+                                  .Configuration
+                                  .GetSection(ScheduleOptions.SectionName)
+                                  .Get<ScheduleOptions>()
+                              ?? throw new InvalidOperationException("Schedule configuration not found");
+
         builder.Services.AddQuartz(q =>
         {
-
             // Repetition Job
             var repetitionJobKey = new JobKey("RepetitionJob");
             q.AddJob<RepetitionJob>(opts => opts.WithIdentity(repetitionJobKey));
-            
-            var repetitionCron = builder.Configuration["Schedule:RepetitionCron"] ?? "0 0 9 * * ?";
+
             q.AddTrigger(opts => opts
                 .ForJob(repetitionJobKey)
                 .WithIdentity("RepetitionJob-trigger")
-                .WithCronSchedule(repetitionCron)
+                .WithCronSchedule(scheduleOptions.RepetitionCron)
                 .WithDescription("Repetition flow trigger"));
 
             // New Words Job
             var newWordsJobKey = new JobKey("NewWordsJob");
             q.AddJob<NewWordsJob>(opts => opts.WithIdentity(newWordsJobKey));
-            
-            var newWordsCron = builder.Configuration["Schedule:NewWordsCron"] ?? "0 0 20 * * ?";
+
             q.AddTrigger(opts => opts
                 .ForJob(newWordsJobKey)
                 .WithIdentity("NewWordsJob-trigger")
-                .WithCronSchedule(newWordsCron)
+                .WithCronSchedule(scheduleOptions.NewWordsCron)
                 .WithDescription("New words flow trigger"));
         });
 
